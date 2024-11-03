b26842e0ef2f02e11478cb997cd92fbec40d0731
Paweł Stiasny <pawelstiasny@gmail.com>
2024-11-03 08:50:14 +0100

    windows

--- src/territory/go.py
+++ src/territory/go.py
@@ -9,7 +9,8 @@ from .api_client import download_resource
 MACHINE = machine().lower()
 if MACHINE == 'x86_64':  MACHINE = 'amd64'
 if MACHINE == 'aarch64':  MACHINE = 'arm64'
-BINARY_KEY = f'goscan-{system().lower()}-{MACHINE}'
+SYSTEM = system().lower()
+BINARY_KEY = f'goscan-{SYSTEM}-{MACHINE}'
 
 
 class Lang:
@@ -27,12 +28,16 @@ class Lang:
 
     def _get_go_scanner(self):
         print('getting parser binary for platform:', BINARY_KEY)
-        bin_path = self.package.temp_dir / 'goscan'
+        if SYSTEM == 'windows':
+            bin_path = self.package.temp_dir / 'goscan.exe'
+        else:
+            bin_path = self.package.temp_dir / 'goscan'
         download_resource(
             upload_token=self.package.upload_token,
             resource=BINARY_KEY,
             destination=bin_path)
-        bin_path.chmod(0o700)
+        if not SYSTEM == 'windows':
+            bin_path.chmod(0o700)
         return bin_path
 
     def _run_go_scanner(self, scan_dir: Path, uim_output_dir: Path, system: bool):
